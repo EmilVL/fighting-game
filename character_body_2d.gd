6b1b277bf871e82a8564223659f732ebf470a0d2
ChristofferS22R <95274166+DamrongS@users.noreply.github.com>
2024-09-04 16:49:34 +0200

    Stickman

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -8,12 +8,10 @@ const MAX_ROTATION_ANGLE = 0.05
 const ACCELERATION = 4200.0
 const DECELERATION = 4200.0
 
-var Acceleration = Vector2(0, 0)
+var hasJumped = false
 
-var hasJumped = false;
-
-# Reference to the Sprite2D node
-var sprite: Sprite2D
+# Reference to the AnimatedSprite2D node
+var sprite: AnimatedSprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -22,22 +20,26 @@ var time_since_last_grounded = 0.0
 var time_since_jump_pressed = JUMP_BUFFER_TIME
 
 func _ready():
-	# Get the Sprite2D node
-	sprite = $Sprite2D
+	# Get the AnimatedSprite2D node
+	sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity*2 * delta
-		time_since_last_grounded += delta
+		velocity.y += gravity * 2 * delta
+		time_since_last_grounded += delta			
+		if velocity.y > 0 and sprite.animation != "fall":
+			sprite.play("fall")
+		
 	else:
 		time_since_last_grounded = 0.0
 		hasJumped = false
-		
+
+		# Perform a buffered jump
 		if time_since_jump_pressed <= JUMP_BUFFER_TIME:
 			hasJumped = true
 			velocity.y = JUMP_VELOCITY
-			
+
 	time_since_jump_pressed += delta
 
 	# Handle jump.
@@ -46,19 +48,32 @@ func _physics_process(delta):
 		if time_since_last_grounded <= COYOTE_TIME and not hasJumped:
 			hasJumped = true
 			velocity.y = JUMP_VELOCITY
+			if velocity.y < 0 and not sprite.is_playing() or sprite.animation != "jump":
+				sprite.play("jump")
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	
+	print(velocity.y)
+
 	if direction != 0:
 		velocity.x = move_toward(velocity.x, direction * SPEED, ACCELERATION * delta)
-
+		
+		# Flip the sprite based on the direction
 		sprite.scale.x = sign(direction) * abs(sprite.scale.x)
+		
+		# Play the running animation if moving horizontally
+		if is_on_floor() and not sprite.is_playing() or sprite.animation != "run" and velocity.y == 0:
+			sprite.play("run")
 	else:
 		# Decelerate toward zero
 		velocity.x = move_toward(velocity.x, 0, DECELERATION * delta)
-		
+
+		# Play the idle animation if not moving horizontally
+		if is_on_floor() and (not sprite.is_playing() or sprite.animation != "idle"):
+			sprite.play("idle")
+
+	# Rotate the sprite slightly based on the velocity
 	sprite.rotation = clamp(velocity.x / SPEED * MAX_ROTATION_ANGLE, -MAX_ROTATION_ANGLE, MAX_ROTATION_ANGLE)
 
 	move_and_slide()
